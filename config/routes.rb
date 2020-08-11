4fe6c22b886edffe86ecc8d4bac1e77539a70854
Patrick Dreier <patrick3dreier@googlemail.com>
2020-08-11 13:48:06 +0200

    DO NOT FOGET THE METHOD

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "tasks", to: "tasks#index"
-  get "tasks/new", to: "tasks#new", as: :new
-  get "tasks/:id", to: "tasks#show", as: :task
-  post "tasks", to: "tasks#create", as: :create
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit
-  patch "tasks/:id", to: "tasks#update", as: :update
-  delete "/tasks/:id", to: "tasks#destroy", as: :delete
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: :new
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create', as: :create
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  delete '/tasks/:id/delete', to: 'tasks#destroy', as: :destroy
+  patch 'tasks/:id', to: 'tasks#update', as: :update
 end
